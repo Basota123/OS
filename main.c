ec6df04634049991964c970d328c1d56edc5bd48
Basota123 <poletaevgena75@gmail.com>
2024-05-19 22:58:01 +0800

    second part for first task

--- main.c
+++ main.c
@@ -1,23 +1,77 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/sem.h>
 
-static int cache[50] = {0};
+#define CACHE_SIZE 50
+#define NUM_PROCESSES 4 // Количество дочерних процессов
 
-int fib(int n)
-{
+static int cache[CACHE_SIZE] = {0};
+int semid; // Дескриптор семафора
+
+int fib(int n) {
     if (n <= 1) return n;
-    if (cache[n] == 0) cache[n] = fib(n-1) + fib(n-2);
+
+    // Захватить семафор
+    struct sembuf sop = {0, -1, 0};
+    semop(semid, &sop, 1);
+
+    if (cache[n] == 0) {
+        cache[n] = fib(n - 1) + fib(n - 2);
+    }
+
+    // Освободить семафор
+    sop.sem_op = 1;
+    semop(semid, &sop, 1);
+
     return cache[n];
 }
 
+int main() {
+    int i, start, end;
+    pid_t pid;
 
-int main()
-{
-
-    for (int i = 1; i < 40; i++)
-    {
-        printf("%d\n", fib(i));
+    // Создаем семафор
+    semid = semget(IPC_PRIVATE, 1, 0666 | IPC_CREAT);
+    if (semid == -1) {
+        perror("semget");
+        exit(1);
     }
 
+    // Инициализируем семафор значением 1
+    semctl(semid, 0, SETVAL, 1);
+
+    // Создаем дочерние процессы
+    for (i = 0; i < NUM_PROCESSES; i++) {
+        pid = fork();
+        if (pid < 0) {
+            perror("fork");
+            exit(1);
+        } else if (pid == 0) {
+            // Дочерний процесс
+            start = i * (CACHE_SIZE / NUM_PROCESSES);
+            end = (i + 1) * (CACHE_SIZE / NUM_PROCESSES);
+            for (int j = start; j < end; j++) {
+                fib(j);
+            }
+            exit(0);
+        }
+    }
+
+    // Родительский процесс
+    for (i = 0; i < NUM_PROCESSES; i++) {
+        wait(NULL); // Ожидаем завершения дочерних процессов
+    }
+
+    // Выводим результаты
+    for (i = 1; i < CACHE_SIZE; i++) {
+        printf("%d\n", cache[i]);
+    }
+
+    // Удаляем семафор
+    semctl(semid, 0, IPC_RMID);
 
     return 0;
 }
--- main.s
+++ main.s
@@ -3,30 +3,49 @@
 	.globl	fib
 	.type	fib, @function
 fib:
-.LFB23:
+.LFB51:
 	.cfi_startproc
 	endbr64
-	movl	%edi, %eax
-	cmpl	$1, %edi
-	jle	.L6
 	pushq	%rbp
 	.cfi_def_cfa_offset 16
 	.cfi_offset 6, -16
 	pushq	%rbx
 	.cfi_def_cfa_offset 24
 	.cfi_offset 3, -24
-	subq	$8, %rsp
-	.cfi_def_cfa_offset 32
+	subq	$24, %rsp
+	.cfi_def_cfa_offset 48
+	movq	%fs:40, %rax
+	movq	%rax, 8(%rsp)
+	xorl	%eax, %eax
+	movl	%edi, %eax
+	cmpl	$1, %edi
+	jle	.L1
 	movl	%edi, %ebx
-	movslq	%edi, %rax
+	movw	$0, 2(%rsp)
+	movw	$-1, 4(%rsp)
+	movw	$0, 6(%rsp)
+	leaq	2(%rsp), %rsi
+	movl	$1, %edx
+	movl	semid(%rip), %edi
+	call	semop@PLT
+	movslq	%ebx, %rax
 	leaq	cache(%rip), %rdx
 	cmpl	$0, (%rdx,%rax,4)
-	je	.L9
+	je	.L7
 .L3:
+	movw	$1, 4(%rsp)
+	leaq	2(%rsp), %rsi
+	movl	$1, %edx
+	movl	semid(%rip), %edi
+	call	semop@PLT
 	movslq	%ebx, %rbx
 	leaq	cache(%rip), %rax
 	movl	(%rax,%rbx,4), %eax
-	addq	$8, %rsp
+.L1:
+	movq	8(%rsp), %rdx
+	subq	%fs:40, %rdx
+	jne	.L8
+	addq	$24, %rsp
 	.cfi_remember_state
 	.cfi_def_cfa_offset 24
 	popq	%rbx
@@ -34,9 +53,9 @@ fib:
 	popq	%rbp
 	.cfi_def_cfa_offset 8
 	ret
-.L9:
+.L7:
 	.cfi_restore_state
-	leal	-1(%rdi), %edi
+	leal	-1(%rbx), %edi
 	call	fib
 	movl	%eax, %ebp
 	leal	-2(%rbx), %edi
@@ -47,80 +66,122 @@ fib:
 	leaq	cache(%rip), %rdx
 	movl	%ebp, (%rdx,%rax,4)
 	jmp	.L3
-.L6:
-	.cfi_def_cfa_offset 8
-	.cfi_restore 3
-	.cfi_restore 6
-	ret
+.L8:
+	call	__stack_chk_fail@PLT
 	.cfi_endproc
-.LFE23:
+.LFE51:
 	.size	fib, .-fib
 	.section	.rodata.str1.1,"aMS",@progbits,1
 .LC0:
+	.string	"semget"
+.LC1:
+	.string	"fork"
+.LC2:
 	.string	"%d\n"
 	.text
 	.globl	main
 	.type	main, @function
 main:
-.LFB24:
+.LFB52:
 	.cfi_startproc
 	endbr64
+	pushq	%r12
+	.cfi_def_cfa_offset 16
+	.cfi_offset 12, -16
 	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
+	.cfi_def_cfa_offset 24
+	.cfi_offset 6, -24
 	pushq	%rbx
-	.cfi_def_cfa_offset 24
-	.cfi_offset 3, -24
-	subq	$8, %rsp
 	.cfi_def_cfa_offset 32
-	movl	$1, %ebx
-	leaq	.LC0(%rip), %rbp
-
-/*
-
-цикл
-
-.L11:
-	movl	%ebx, %edi
-	call	fib
-	movl	%eax, %edx
+	.cfi_offset 3, -32
+	movl	$950, %edx
+	movl	$1, %esi
+	movl	$0, %edi
+	call	semget@PLT
+	movl	%eax, semid(%rip)
+	cmpl	$-1, %eax
+	je	.L20
+	movl	%eax, %edi
+	movl	$1, %ecx
+	movl	$16, %edx
+	movl	$0, %esi
+	movl	$0, %eax
+	call	semctl@PLT
+	movl	$0, %ebx
+.L14:
+	call	fork@PLT
+	testl	%eax, %eax
+	js	.L21
+	je	.L22
+	addl	$1, %ebx
+	cmpl	$4, %ebx
+	jne	.L14
+	movl	$0, %edi
+	call	wait@PLT
+	movl	$0, %edi
+	call	wait@PLT
+	movl	$0, %edi
+	call	wait@PLT
+	movl	$0, %edi
+	call	wait@PLT
+	leaq	4+cache(%rip), %rbx
+	leaq	196(%rbx), %r12
+	leaq	.LC2(%rip), %rbp
+.L15:
+	movl	(%rbx), %edx
 	movq	%rbp, %rsi
 	movl	$1, %edi
 	movl	$0, %eax
 	call	__printf_chk@PLT
-	addl	$1, %ebx
-	cmpl	$40, %ebx
-	jne	.L11
-
-	Переменные:
-    	 `%ebx` - счетчик цикла.
-    	 `%rbp` - указатель на строку формата для вывода (`%d\n`).
-    	 `%edi` - аргумент функции `fib`.
-
-*/
-
-.L11:
-	movl	%ebx, %edi
-	call	fib
-	movl	%eax, %edx
-	movq	%rbp, %rsi
-	movl	$1, %edi
+	addq	$4, %rbx
+	cmpq	%rbx, %r12
+	jne	.L15
+	movl	$0, %edx
+	movl	$0, %esi
+	movl	semid(%rip), %edi
 	movl	$0, %eax
-	call	__printf_chk@PLT
-	addl	$1, %ebx
-	cmpl	$40, %ebx
-	jne	.L11
+	call	semctl@PLT
 	movl	$0, %eax
-	addq	$8, %rsp
-	.cfi_def_cfa_offset 24
 	popq	%rbx
-	.cfi_def_cfa_offset 16
+	.cfi_remember_state
+	.cfi_def_cfa_offset 24
 	popq	%rbp
+	.cfi_def_cfa_offset 16
+	popq	%r12
 	.cfi_def_cfa_offset 8
 	ret
+.L20:
+	.cfi_restore_state
+	leaq	.LC0(%rip), %rdi
+	call	perror@PLT
+	movl	$1, %edi
+	call	exit@PLT
+.L21:
+	leaq	.LC1(%rip), %rdi
+	call	perror@PLT
+	movl	$1, %edi
+	call	exit@PLT
+.L22:
+	imull	$12, %ebx, %ebx
+	leal	12(%rbx), %ebp
+.L13:
+	movl	%ebx, %edi
+	call	fib
+	addl	$1, %ebx
+	cmpl	%ebx, %ebp
+	jg	.L13
+	movl	$0, %edi
+	call	exit@PLT
 	.cfi_endproc
-.LFE24:
+.LFE52:
 	.size	main, .-main
+	.globl	semid
+	.bss
+	.align 4
+	.type	semid, @object
+	.size	semid, 4
+semid:
+	.zero	4
 	.local	cache
 	.comm	cache,200,32
 	.ident	"GCC: (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0"
